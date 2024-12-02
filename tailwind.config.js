19377bc78225a151e0c69dbd7e770bac2cbe2e2d
Arun96980 <prasantarun28@gmail.com>
2024-12-02 15:36:35 +0530

    web design

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      red: ['Sixtyfour Convergence' , 'sans-serif']
+    }
+  },
+  plugins: [],
+  darkMode: "selector",
+  
+}
+
